230fb43e930439fc1dcafe519221adb186cd7f4b
Andhikaryan656@gmail.com <Andhikaryan656@gmail.com>
2025-06-13 08:20:57 +0700

    Menambahkan pengecekan underflow pada queue

--- circularQueques.cpp
+++ circularQueques.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class Queques
+class Queue 
 {
-    int FRONT, REAR, max =5;
-    int queque_array[5];
+    int FRONT, REAR, max = 5;
+    int queue_array[5];  
 
 public:
-    QueQues()
+    Queue()  
     {
         FRONT = -1;
         REAR = -1;
@@ -16,31 +16,40 @@ public:
     void insert()
     {
         int num;
-        cout << "Enter a number:";
+        cout << "Enter a number: ";
         cin >> num;
         cout << endl;
 
-        //cek apakah antrian penuh
-        if (( FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
-          cout << "\nQueque overflow\n";
-          return
+            cout << "\nQueue overflow\n";
+            return; 
         }
 
-        //cek apakah antrian kosong
+        // cek apakah antrian kosong
         if (FRONT == -1)
         {
             FRONT = 0;
             REAR = 0;
         }
-        
-        else{
-            //jika REAR berada di posisi terakhir array ,kembali ke awal array
-            if (REAR == max -1)
+        else
+        {
+            // jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
                 REAR = 0;
-            else  
-               REAR = REAR + 1;
+            else
+                REAR = REAR + 1;
         }
-    }
 
-}
+        queue_array[REAR] = num;
+    }
+    void remove(){
+        if (FRONT == -1){
+            cout << "Queque underflow\n";
+            return;
+        }
+
+        
+    }
+};
